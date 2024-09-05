bf7a305463b43677ab8cbbffd5d910d1aef90954
Camilo <Camilo@DESKTOP-C8U30HS>
2024-09-05 12:21:22 -0600

    Add project files.

---
+++ ItaliasPizza/Pages/Login.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliasPizza.Pages
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterKeyEvent(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                //Application.Current.MainWindow.Content = new MainMenu();
+                //todo
+            }
+        }
+
+        private void BtnLoginEvent(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancelEvent(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LblResetPasswordEvent(object sender, MouseEventArgs e)
+        {
+            //todo
+        }
+    }
+}
---
+++ ItaliasPizza/Pages/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ItaliasPizza.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliasPizza
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResizeMode = ResizeMode.NoResize;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            //Closing += OnWindowClosing;
+            Login login = new Login();
+            Content = login;
+        }
+    }
+}
