a93d6a3d3f6d98d366fa47bf7b782add47e5a3d7
bryamdmg <bryamdmg@gmail.com>
2024-09-29 16:13:46 -0600

    validations for empty fields and datepickers for SupplierOrderRegister

--- ItaliasPizza/Pages/SupplierOrderRegister.xaml.cs
+++ ItaliasPizza/Pages/SupplierOrderRegister.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace ItaliasPizza.Pages
 {
@@ -23,11 +25,40 @@ namespace ItaliasPizza.Pages
         public SupplierOrderRegister()
         {
             InitializeComponent();
+            CbCategory.ItemsSource = GetCategories();
+        }
+
+        private List<String> GetCategories()
+        {
+            using (var db = new ItaliasPizzaDBEntities())
+            {
+                return db.SupplyCategory.Select(c => c.SupplyCategory1).ToList();
+            }
+        }
+
+        private bool AreFieldsFilled()
+        {
+            return !string.IsNullOrEmpty(DtpOrder.Text)
+                && !string.IsNullOrEmpty(DtpEstimatedArrival.Text)
+                && !string.IsNullOrEmpty(CbSupplier.Text)
+                && !string.IsNullOrEmpty(CbCategory.Text)
+                && !string.IsNullOrEmpty(TxtAmount.Text);
+        }
+
+        private bool IsDateValid()
+        {
+            return DtpOrder.SelectedDate < DtpEstimatedArrival.SelectedDate;
         }
 
         private void Btn_Save(object sender, RoutedEventArgs e)
         {
-
+            if (!AreFieldsFilled())
+            {
+                MessageBox.Show("Todos los campos deben contener información");
+            } else if (IsDateValid())
+            {
+                MessageBox.Show("Las fecha estimada de llegada debe ser posterior a la fecha del pedido");
+            }
         }
 
         private void Btn_Cancel(object sender, RoutedEventArgs e)
