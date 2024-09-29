a0ed52282281ea28c23713938dafeb1fdf93af66
bryamdmg <bryamdmg@gmail.com>
2024-09-29 11:15:56 -0600

    validation for empty fields for SupplyModification

--- ItaliasPizza/Pages/SupplyModification.xaml.cs
+++ ItaliasPizza/Pages/SupplyModification.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,48 @@ namespace ItaliasPizza.Pages
         public SupplyModification()
         {
             InitializeComponent();
+            CbCategory.ItemsSource = GetCategories();
+        }
+
+        private List<String> GetCategories()
+        {
+            using (var db = new ItaliasPizzaDBEntities())
+            {
+                return db.SupplyCategory.Select(c => c.SupplyCategory1).ToList();
+            }
+        }
+
+        private bool AreFieldsFilled()
+        {
+            return !string.IsNullOrEmpty(TxtName.Text)
+                && !string.IsNullOrEmpty(TxtAmount.Text)
+                && !string.IsNullOrEmpty(DtpExpiration.Text)
+                && !string.IsNullOrEmpty(CbCategory.Text);
+        }
+
+        private void Btn_Save(object sender, RoutedEventArgs e)
+        {
+            if (!AreFieldsFilled())
+            {
+                MessageBox.Show("Todos los campos deben contener información");
+            }
+        }
+
+        private void Btn_Eliminate(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("¿Está seguro que desea eliminar este insumo?",
+                                                      "Advertencia",
+                                                      MessageBoxButton.YesNo,
+                                                      MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+
+        private void Btn_Cancel(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
