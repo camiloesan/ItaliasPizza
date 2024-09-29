1bdc60c46643a8871edfdbfe5554db679c447d39
bryamdmg <bryamdmg@gmail.com>
2024-09-29 16:05:33 -0600

    page to register suppliers orders created

---
+++ ItaliasPizza/Pages/SupplierOrderRegister.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliasPizza.Pages
+{
+    /// <summary>
+    /// Interaction logic for SupplierOrderRegister.xaml
+    /// </summary>
+    public partial class SupplierOrderRegister : Page
+    {
+        public SupplierOrderRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Cancel(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
