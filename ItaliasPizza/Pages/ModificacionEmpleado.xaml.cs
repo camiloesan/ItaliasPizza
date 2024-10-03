6648ed145819a2de2d9067c761c869412c1fe805
camilo <camiloesan@gmail.com>
2024-10-03 11:23:40 -0600

    ?

--- ItaliasPizza/Pages/ModificacionEmpleado.xaml.cs
+++ ItaliasPizza/Pages/ModificacionEmpleado.xaml.cs
@@ -23,6 +23,24 @@ namespace ItaliasPizza.Pages
             CbCharge.ItemsSource = GetCharges();
         }
 
+        private void ResetTextFormBorders()
+        {
+            TxtName.BorderBrush = Brushes.Black;
+            TxtName.BorderThickness = new Thickness(1);
+            TxtLastName.BorderBrush = Brushes.Black;
+            TxtLastName.BorderThickness = new Thickness(1);
+            TxtPhone.BorderBrush = Brushes.Black;
+            TxtPhone.BorderThickness = new Thickness(1);
+            TxtEmail.BorderBrush = Brushes.Black;
+            TxtEmail.BorderThickness = new Thickness(1);
+            TxtPassword.BorderBrush = Brushes.Black;
+            TxtPassword.BorderThickness = new Thickness(1);
+            CbStatus.BorderBrush = Brushes.Black;
+            CbStatus.BorderThickness = new Thickness(1);
+            CbCharge.BorderBrush = Brushes.Black;
+            CbCharge.BorderThickness = new Thickness(1);
+        }
+
         private void UpdateEmployee(Guid employeeId)
         {
             HighlightInvalidFields();
@@ -185,111 +203,6 @@ namespace ItaliasPizza.Pages
                 TxtEmail.BorderBrush = Brushes.Red;
                 TxtEmail.BorderThickness = new Thickness(2);
             }
-
-        private List<Charge> GetCharges()
-        {
-            using (var db = new ItaliasPizzaDBEntities())
-            {
-                return db.Charge.ToList();
-            }
-        }
-
-        private void FillFields(Employee employee)
-        {
-            string employeeEmail = EmployeeOperations.GetEmployeeEmail(employee.IdEmployee);
-            TxtName.Text = employee.FirstName;
-            TxtLastName.Text = employee.LastName;
-            TxtPhone.Text = employee.Phone;
-            TxtEmail.Text = employeeEmail;
-            CbCharge.SelectedIndex = employee.IdCharge;
-            TxtPassword.Password = AccessAccountOperations.GetEmployeePassword(employeeEmail);
-            if (employee.Status)
-            {
-                CbStatus.SelectedIndex = 0;
-            }
-            else
-            {
-                CbStatus.SelectedIndex = 1;
-            }
-        }
-        private bool AreFieldsFilled()
-        {
-            return !string.IsNullOrEmpty(TxtName.Text)
-                && !string.IsNullOrEmpty(TxtLastName.Text)
-                && !string.IsNullOrEmpty(TxtPhone.Text)
-                && !string.IsNullOrEmpty(CbCharge.Text)
-                && !string.IsNullOrEmpty(TxtEmail.Text)
-                && !string.IsNullOrEmpty(TxtPassword.Password)
-                && CbStatus.SelectedIndex != 0;
-        }
-
-        public bool IsEmailValid(string email)
-        {
-            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
-            return regex.IsMatch(email);
-        }
-
-        private void HighlightInvalidFields()
-        {
-            string name = TxtName.Text.Trim();
-            string lastName = TxtLastName.Text.Trim();
-            string phone = TxtPhone.Text.Trim();
-            string email = TxtEmail.Text.Trim();
-            string password = TxtPassword.Password.Trim();
-            ResetTextFormBorders();
-
-            bool isPhoneRegistered = EmployeeOperations.IsPhoneRegistered(phone);
-            bool isEmailRegistered = EmployeeOperations.IsEmailRegistered(email);
-            bool isEmailValid = IsEmailValid(email);
-
-            if (string.IsNullOrEmpty(name))
-            {
-                TxtName.BorderBrush = Brushes.Red;
-                TxtName.BorderThickness = new Thickness(2);
-            }
-
-            if (string.IsNullOrEmpty(lastName))
-            {
-                TxtLastName.BorderBrush = Brushes.Red;
-                TxtLastName.BorderThickness = new Thickness(2);
-            }
-
-            if (string.IsNullOrEmpty(phone) || isPhoneRegistered)
-            {
-                TxtPhone.BorderBrush = Brushes.Red;
-                TxtPhone.BorderThickness = new Thickness(2);
-            }
-
-            if (string.IsNullOrEmpty(email) || !isEmailValid || !isEmailRegistered)
-            {
-                TxtEmail.BorderBrush = Brushes.Red;
-                TxtEmail.BorderThickness = new Thickness(2);
-            }
-
-            if (string.IsNullOrEmpty(password))
-            {
-                TxtPassword.BorderBrush = Brushes.Red;
-                TxtPassword.BorderThickness = new Thickness(2);
-            }
-        }
-
-        private void ResetTextFormBorders()
-        {
-            TxtName.BorderBrush = Brushes.Black;
-            TxtName.BorderThickness = new Thickness(1);
-            TxtLastName.BorderBrush = Brushes.Black;
-            TxtLastName.BorderThickness = new Thickness(1);
-            TxtPhone.BorderBrush = Brushes.Black;
-            TxtPhone.BorderThickness = new Thickness(1);
-            TxtEmail.BorderBrush = Brushes.Black;
-            TxtEmail.BorderThickness = new Thickness(1);
-            TxtPassword.BorderBrush = Brushes.Black;
-            TxtPassword.BorderThickness = new Thickness(1);
-            CbStatus.BorderBrush = Brushes.Black;
-            CbStatus.BorderThickness = new Thickness(1);
-            CbCharge.BorderBrush = Brushes.Black;
-            CbCharge.BorderThickness = new Thickness(1);
         }
     }
 }
