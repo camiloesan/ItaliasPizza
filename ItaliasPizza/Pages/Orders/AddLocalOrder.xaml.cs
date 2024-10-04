04a6fe461e8e998ba04e5fa5cf2ed531c0023fc1
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-10-03 23:47:18 -0600

    Add Local Order View

---
+++ ItaliasPizza/Pages/Orders/AddLocalOrder.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliasPizza.Pages.Orders
+{
+	/// <summary>
+	/// Interaction logic for AddLocalOrder.xaml
+	/// </summary>
+	public partial class AddLocalOrder : Page
+	{
+		public AddLocalOrder()
+		{
+			InitializeComponent();
+			var items = new ObservableCollection<Product>()
+			{
+				new Product {Name = "Pizza", Price = 100},
+			};
+			DtgActiveProducts.ItemsSource = items;
+		}
+
+		private void BtnFinishOrder(object sender, MouseButtonEventArgs e)
+		{
+
+        }
+
+		private void BtnCancelOrder(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+
+		private void ImgReturn(object sender, MouseButtonEventArgs e)
+		{
+			
+		}
+	}
+}
