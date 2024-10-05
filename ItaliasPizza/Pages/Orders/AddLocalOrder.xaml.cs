0168c1c195cb91240b128d0f17bcb38d96cffe26
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-10-05 13:18:42 -0600

    Add local order view

--- ItaliasPizza/Pages/Orders/AddLocalOrder.xaml.cs
+++ ItaliasPizza/Pages/Orders/AddLocalOrder.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Database;
+using ItaliasPizza.DataAccessLayer;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -25,26 +26,75 @@ namespace ItaliasPizza.Pages.Orders
 		public AddLocalOrder()
 		{
 			InitializeComponent();
-			var items = new ObservableCollection<Product>()
+			/*var items = new ObservableCollection<Product>()
 			{
 				new Product {Name = "Pizza", Price = 100},
 			};
-			DtgActiveProducts.ItemsSource = items;
+			DtgActiveProducts.ItemsSource = items;*/
+			FillProductList();
+		}
+
+		private void BtnAddProduct_Click(object sender, RoutedEventArgs e)
+		{
+			Button button = (Button)sender;
+			Product selectedProduct = (Product)button.DataContext;
+			AddProductToOrder(selectedProduct);
+		}
+
+		private void RemoveProduct_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 
 		private void BtnFinishOrder(object sender, MouseButtonEventArgs e)
 		{
-
+			
         }
 
 		private void BtnCancelOrder(object sender, MouseButtonEventArgs e)
 		{
-
+			
 		}
 
 		private void ImgReturn(object sender, MouseButtonEventArgs e)
 		{
 			
 		}
+
+		private void AddProductToOrder(Product selectedProduct)
+		{
+			if (EnoughSupplies(selectedProduct))
+			{
+				var selectedProducts = new List<Product>(); //TODO: Que se pueda agregar más de un producto
+				selectedProducts.Add(selectedProduct);
+				FillSelectedProductsList(selectedProducts);
+			}
+			else
+			{
+				MessageBox.Show("No hay suficientes insumos para más de este producto.");
+			}
+		}
+
+		private bool EnoughSupplies(Product selectedProduct)
+		{
+			return ProductOperations.GetPreparableProductQuantity(selectedProduct) > 0;
+		}
+
+		private void FillSelectedProductsList(List<Product> selectedProducts)
+		{
+			var items = new ObservableCollection<Product>(selectedProducts);
+			DtgSelectedProducts.ItemsSource = items;
+		}
+
+		private void FillProductList()
+		{
+			// Fill product list
+			var activeProducts = ProductOperations.GetActiveProducts();
+
+			var items = new ObservableCollection<Product>(activeProducts);
+			DtgActiveProducts.ItemsSource = items;
+		}
+
+		
 	}
 }
