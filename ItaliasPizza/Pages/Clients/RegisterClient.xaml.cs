296bb93027b135a315b28dc7dbd5c6a87ef75ae2
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-10-05 17:40:16 -0600

    Register client view

---
+++ ItaliasPizza/Pages/Clients/RegisterClient.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliasPizza.Pages.Clients
+{
+	/// <summary>
+	/// Interaction logic for RegisterClient.xaml
+	/// </summary>
+	public partial class RegisterClient : Page
+	{
+		public RegisterClient()
+		{
+			InitializeComponent();
+		}
+
+		private void Btn_Save(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Btn_Cancel(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
