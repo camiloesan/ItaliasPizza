ec1a1ea3ec079574d30437ce3f4905d22d8d0107
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-10-08 11:58:32 -0600

    Update unit tests

--- ItaliasPizzaTests/DataAccessLayer/ProductOperationsTests.cs
+++ ItaliasPizzaTests/DataAccessLayer/ProductOperationsTests.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 
 namespace ItaliasPizzaTests.DataAccessLayer
@@ -64,80 +65,228 @@ namespace ItaliasPizzaTests.DataAccessLayer
 		[TestMethod]
 		public void GetACtiveProductsTest()
 		{
+			var idMargarita = Guid.NewGuid();
+			var idPiPepperoni = Guid.NewGuid();
+			var idMexicana = Guid.NewGuid();
+			var idVegetariana = Guid.NewGuid();
+			var idHawaiana = Guid.NewGuid();
+
+			var margarita = new Product { IdProduct = idMargarita, Name = "Pizza Margarita", IdType = 1, Price = 100, Size = "Chica", Status = true };
+			var pepperoni = new Product { IdProduct = idPiPepperoni, Name = "Pizza Pepperoni", IdType = 1, Price = 120, Size = "Chica", Status = true };
+			var mexicana = new Product { IdProduct = idMexicana, Name = "Pizza Mexicana", IdType = 1, Price = 140, Size = "Chica", Status = true };
+			var vegetariana = new Product { IdProduct = Guid.NewGuid(), Name = "Pizza Vegetariana", IdType = 1, Price = 130, Size = "Chica", Status = false };
+			var hawaiana = new Product { IdProduct = Guid.NewGuid(), Name = "Pizza Hawaiana", IdType = 1, Price = 150, Size = "Chica", Status = false };
+
 			var expected = new List<Product>
 			{
-				// Los GUID varian en cada base de datos, por lo que se deben cambiar
-				// TODO: constructor de datos de prueba para que los GUID sean iguales
-				new Product
-				{
-					IdProduct = Guid.Parse("e4021288-5e41-4385-9b26-3b4f294fa5eb"),
-					Name = "Pizza Margarita",
-					IdType = 1,
-					Price = 100,
-					Status = true
-				},
-				new Product
-				{
-					IdProduct = Guid.Parse("e378aeca-3c97-4ca1-ad86-c81f47fa6e4d"),
-					Name = "Pizza Pepperoni",
-					IdType = 1,
-					Price = 120,
-					Status = true
-				},
-				new Product
-				{
-					IdProduct = Guid.Parse("d401dbf2-cc35-4abd-9156-e67076f3708a"),
-					Name = "Pizza Mexicana",
-					IdType = 1,
-					Price = 140,
-					Status = true
-				},
-				new Product
-				{
-					IdProduct = Guid.Parse("6fb66d23-7e34-4572-ab34-ea5030def8ac"),
-					Name = "Pizza Vegetariana",
-					IdType = 1,
-					Price = 110,
-					Status = true
-				},
-				new Product
-				{
-					IdProduct = Guid.Parse("ba7aafa9-d418-40fd-af76-eea03f4d979c"),
-					Name = "Pizza Hawaiana",
-					IdType = 1,
-					Price = 130,
-					Status = true
-				}
+				margarita,
+				pepperoni,
+				mexicana
 			};
+
+			using (var db = new ItaliasPizzaDBEntities())
+			{
+				foreach (var product in expected)
+				{
+					ProductOperations.SaveProduct(product);
+				}
+			}
 
 			var result = ProductOperations.GetActiveProducts().ToList();
 
-			//Assert.AreEqual(expected, result);
-			for (int i = 0; i < expected.Count; i++)
+			using (var db = new ItaliasPizzaDBEntities())
 			{
-				Assert.AreEqual(expected[i].IdProduct, result[i].IdProduct);
-				Assert.AreEqual(expected[i].Name, result[i].Name);
-				Assert.AreEqual(expected[i].IdType, result[i].IdType);
-				Assert.AreEqual(expected[i].Price, result[i].Price);
-				Assert.AreEqual(expected[i].Status, result[i].Status);
+				db.Product.Attach(margarita);
+				db.Product.Attach(pepperoni);
+				db.Product.Attach(mexicana);
+				db.Product.Remove(margarita);
+				db.Product.Remove(pepperoni);
+				db.Product.Remove(mexicana);
+
+				db.SaveChanges();
+			}
+
+			foreach (var product in expected)
+			{
+				Assert.IsTrue(result.Any(p => p.IdProduct == product.IdProduct));
 			}
 		}
 
 		[TestMethod]
 		public void GetPreparableProductQuantityTest()
 		{
-			// Los GUID varian en cada base de datos, por lo que se deben cambiar
-			// TODO: constructor de datos de prueba para que los GUID sean igualess
-			var stringIdProduct = "e4021288-5e41-4385-9b26-3b4f294fa5eb";
-			var idProduct = Guid.Parse(stringIdProduct);
-
-			Product product = new Product
-			{
-				IdProduct = idProduct
+			var idMargarita = Guid.NewGuid();
+			var idPiPepperoni = Guid.NewGuid();
+			var idMexicana = Guid.NewGuid();
+			var idVegetariana = Guid.NewGuid();
+			var idHawaiana = Guid.NewGuid();
+			var margarita = new Product { IdProduct = idMargarita, Name = "Pizza Margarita", IdType = 1, Price = 100, Size = "Chica", Status = true };
+			var pepperoni = new Product { IdProduct = idPiPepperoni, Name = "Pizza Pepperoni", IdType = 1, Price = 120, Size = "Chica", Status = true };
+			var mexicana = new Product { IdProduct = idMexicana, Name = "Pizza Mexicana", IdType = 1, Price = 140, Size = "Chica", Status = true };
+			var vegetariana = new Product { IdProduct = idVegetariana, Name = "Pizza Vegetariana", IdType = 1, Price = 130, Size = "Chica", Status = false };
+			var hawaiana = new Product { IdProduct = idHawaiana, Name = "Pizza Hawaiana", IdType = 1, Price = 150, Size = "Chica", Status = false };
+			var pizzas = new List<Product>
+			{               
+				margarita,
+				pepperoni,
+				mexicana,
+				vegetariana,
+				hawaiana
 			};
+			foreach (var pizza in pizzas)
+			{
+				ProductOperations.SaveProduct(pizza);
+			}
+
+			var idHarina = Guid.NewGuid();
+			var idQueso = Guid.NewGuid();
+			var idTomate = Guid.NewGuid();
+			var idPepperoni = Guid.NewGuid();
+			var idPina = Guid.NewGuid();
+			var idChile = Guid.NewGuid();
+			var supHarina = new Supply { IdSupply = idHarina, Name = "Harina", Quantity = 100.00m, IdSupplyCategory = 1, IdMeasurementUnit = 1, Status = true };
+			var supQueso = new Supply { IdSupply = idQueso, Name = "Queso", Quantity = 50.00m, IdSupplyCategory = 3, IdMeasurementUnit = 1, Status = true };
+			var supTomate = new Supply { IdSupply = idTomate, Name = "Tomate", Quantity = 30.00m, IdSupplyCategory = 6, IdMeasurementUnit = 1, Status = true };
+			var supPeppeorni = new Supply { IdSupply = idPepperoni, Name = "Pepperoni", Quantity = 20.00m, IdSupplyCategory = 4, IdMeasurementUnit = 1, Status = true };
+			var supPina = new Supply { IdSupply = idPina, Name = "Piña", Quantity = 10.00m, IdSupplyCategory = 6, IdMeasurementUnit = 1, Status = true };
+			var supChile = new Supply { IdSupply = idChile, Name = "Chile", Quantity = 10.00m, IdSupplyCategory = 6, IdMeasurementUnit = 1, Status = true };
+			var supplies = new List<Supply>
+			{
+				supHarina,
+				supQueso,
+				supTomate,
+				supPeppeorni,
+				supPina,
+				supChile
+			};
+			using (var db = new ItaliasPizzaDBEntities())
+			{
+				foreach (var supply in supplies)
+				{
+					SupplyOperations.SaveSupply(supply);
+				}
+			}
+
+			var recMargarita = Guid.NewGuid();
+			var recPiPepperoni = Guid.NewGuid();
+			var recHawaiana = Guid.NewGuid();
+			var recMexicana = Guid.NewGuid();
+			var recVegetariana = Guid.NewGuid();
+			var reciMargarita = new Recipe { IdRecipe = recMargarita, IdProduct = idMargarita, Instructions = "Mezclar la harina con el agua y la levadura, dejar reposar por 1 hora" };
+			var reciPepperoni = new Recipe { IdRecipe = recPiPepperoni, IdProduct = idPiPepperoni, Instructions = "Mezclar la harina con el agua y la levadura, dejar reposar por 1 hora, agregar el pepperoni" };
+			var reciHawaiana = new Recipe { IdRecipe = recHawaiana, IdProduct = idHawaiana, Instructions = "Mezclar la harina con el agua y la levadura, dejar reposar por 1 hora, agregar la piña" };
+			var reciVegetariana = new Recipe { IdRecipe = recVegetariana, IdProduct = idVegetariana, Instructions = "Mezclar la harina con el agua y la levadura, dejar reposar por 1 hora, agregar los vegetales" };
+			var reciMexicana = new Recipe { IdRecipe = recMexicana, IdProduct = idMexicana, Instructions = "Mezclar la harina con el agua y la levadura, dejar reposar por 1 hora, agregar el chile" };
+			var recipes = new List<Recipe>
+			{
+				reciMargarita,
+				reciPepperoni,
+				reciHawaiana,
+				reciVegetariana,
+				reciMexicana
+			};
+			using (var db = new ItaliasPizzaDBEntities()) 
+			{
+				foreach (var recipe in recipes)
+				{
+					RecipeOperations.SaveRecipe(recipe);
+				}
+			}
+
+			var recSup1Margarita = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recMargarita, IdSupply = idHarina, SupplyAmount = 1.00m, IdMeasurementUnit = 1 };
+			var recSup2Margarita = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recMargarita, IdSupply = idQueso, SupplyAmount = 0.50m, IdMeasurementUnit = 1 };
+			var recSup1Pepperoni = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recPiPepperoni, IdSupply = idHarina, SupplyAmount = 1.00m, IdMeasurementUnit = 1 };
+			var recSup2Pepperoni = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recPiPepperoni, IdSupply = idPepperoni, SupplyAmount = 0.20m, IdMeasurementUnit = 1 };
+			var recSup1Hawaiana = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recHawaiana, IdSupply = idHarina, SupplyAmount = 1.00m, IdMeasurementUnit = 1 };
+			var recSup2Hawaiana = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recHawaiana, IdSupply = idPina, SupplyAmount = 0.30m, IdMeasurementUnit = 1 };
+			var recSup1Vegetariana = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recVegetariana, IdSupply = idHarina, SupplyAmount = 1.00m, IdMeasurementUnit = 1 };
+			var recSup2Vegetariana = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recVegetariana, IdSupply = idTomate, SupplyAmount = 0.50m, IdMeasurementUnit = 1 };
+			var recSup1Mexicana = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recMexicana, IdSupply = idHarina, SupplyAmount = 1.00m, IdMeasurementUnit = 1 };
+			var recSup2Mexicana = new RecipeSupply { IdRecipeSupply = Guid.NewGuid(), IdRecipe = recMexicana, IdSupply = idChile, SupplyAmount = 0.20m, IdMeasurementUnit = 1 };
+			var recipeSupplies = new List<RecipeSupply>
+			{
+				recSup1Margarita,
+				recSup2Margarita,
+				recSup1Pepperoni,
+				recSup2Pepperoni,
+				recSup1Hawaiana,
+				recSup2Hawaiana,
+				recSup1Vegetariana,
+				recSup2Vegetariana,
+				recSup1Mexicana,
+				recSup2Mexicana
+			};
+			using (var db = new ItaliasPizzaDBEntities())
+			{
+				db.RecipeSupply.AddRange(recipeSupplies);
+				db.SaveChanges();
+			}
 
 			var expected = 100;
-			var result = ProductOperations.GetPreparableProductQuantity(product);
+			var result = ProductOperations.GetPreparableProductQuantity(pepperoni);
+
+			using (var db = new ItaliasPizzaDBEntities())
+			{
+				db.Product.Attach(margarita);
+				db.Product.Attach(pepperoni);
+				db.Product.Attach(mexicana);
+				db.Product.Attach(vegetariana);
+				db.Product.Attach(hawaiana);
+				db.Product.Remove(margarita);
+				db.Product.Remove(pepperoni);
+				db.Product.Remove(mexicana);
+				db.Product.Remove(vegetariana);
+				db.Product.Remove(hawaiana);
+
+				db.Supply.Attach(supHarina);
+				db.Supply.Attach(supQueso);
+				db.Supply.Attach(supTomate);
+				db.Supply.Attach(supPeppeorni);
+				db.Supply.Attach(supPina);
+				db.Supply.Attach(supChile);
+				db.Supply.Remove(supHarina);
+				db.Supply.Remove(supQueso);
+				db.Supply.Remove(supTomate);
+				db.Supply.Remove(supPeppeorni);
+				db.Supply.Remove(supPina);
+				db.Supply.Remove(supChile);
+				
+
+				db.Recipe.Attach(reciMargarita);
+				db.Recipe.Attach(reciPepperoni);
+				db.Recipe.Attach(reciHawaiana);
+				db.Recipe.Attach(reciVegetariana);
+				db.Recipe.Attach(reciMexicana);
+				db.Recipe.Remove(reciMargarita);
+				db.Recipe.Remove(reciPepperoni);
+				db.Recipe.Remove(reciHawaiana);
+				db.Recipe.Remove(reciVegetariana);
+				db.Recipe.Remove(reciMexicana);
+
+				db.RecipeSupply.Attach(recSup1Margarita);
+				db.RecipeSupply.Attach(recSup2Margarita);
+				db.RecipeSupply.Attach(recSup1Pepperoni);
+				db.RecipeSupply.Attach(recSup2Pepperoni);
+				db.RecipeSupply.Attach(recSup1Hawaiana);
+				db.RecipeSupply.Attach(recSup2Hawaiana);
+				db.RecipeSupply.Attach(recSup1Vegetariana);
+				db.RecipeSupply.Attach(recSup2Vegetariana);
+				db.RecipeSupply.Attach(recSup1Mexicana);
+				db.RecipeSupply.Attach(recSup2Mexicana);
+				db.RecipeSupply.Remove(recSup1Margarita);
+				db.RecipeSupply.Remove(recSup2Margarita);
+				db.RecipeSupply.Remove(recSup1Pepperoni);
+				db.RecipeSupply.Remove(recSup2Pepperoni);
+				db.RecipeSupply.Remove(recSup1Hawaiana);
+				db.RecipeSupply.Remove(recSup2Hawaiana);
+				db.RecipeSupply.Remove(recSup1Vegetariana);
+				db.RecipeSupply.Remove(recSup2Vegetariana);
+				db.RecipeSupply.Remove(recSup1Mexicana);
+				db.RecipeSupply.Remove(recSup2Mexicana);
+
+				db.SaveChanges();
+			}
+
 			Assert.AreEqual(expected, result);
 		}
 	}
